97b659659ea0698b7158c16c4c186d1644007a2e
Eeveelution <developerbrzoza@gmail.com>
2021-08-02 15:06:53 +0200

    alot of linux code

--- UsageTracker/Program.cs
+++ UsageTracker/Program.cs
@@ -4,7 +4,10 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading;
+using UsageTracker;
 using UsageTracker.Common;
+
+Platform.Initialize();
 
 List<TrackedProcess> trackedProcesses = new();
 
